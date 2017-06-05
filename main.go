971915ecf96499f48484ca6e3620b19c83d11fbd
nubix <nubix@office.dockter.com>
2017-06-04 23:01:13 -0400

    Added comments for upcoming work.

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "fmt"
 var gitVersion string = "0.0.0"
 var gitIteration string = "0"
 
+// TODO: Add docopt.
+// TODO: Add logging.
+// TODO: Add subcommand structure.
+// TODO: RPC plugin system - https://github.com/hashicorp/go-plugin
+
 func main() {
     fmt.Printf("Hello, world! from Version %s-%s\n", gitVersion, gitIteration)
 }
